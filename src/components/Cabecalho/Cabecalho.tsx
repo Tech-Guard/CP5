b60002e4e9c8c09e0adfd0bb788003c85397d5a9
JulioDev01 <juliocesarmaciel21@gmail.com>
2024-10-20 23:08:56 -0300

    Adicionando o Logo da NavBar

--- src/components/Cabecalho/Cabecalho.tsx
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -3,6 +3,16 @@ import Menu from "../Menu/Menu";
 
 export default function Cabecalho() {
   return (
-
+    <header className="bg-gray-800 p-4 flex justify-between items-center">
+      <div className="flex items-center">
+        <Image 
+          src="/img/logoIcon.png" 
+          alt="Logo"
+          width={50}
+          height={50}
+          className="mr-2"
+        />
+      </div>
+    </header>
   );
 }
