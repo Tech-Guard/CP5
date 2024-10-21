c90cdcd9a28bc530c0400f3ed2d72cd9c0c5ee38
JulioDev01 <juliocesarmaciel21@gmail.com>
2024-10-20 23:05:22 -0300

    Adicionando o Menu com as infos do NavBar

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -2,6 +2,13 @@ import Link from 'next/link';
 
 export default function Menu() {
   return (
-
+    <nav className="bg-blue-500 p-4 rounded-lg">
+      <ul className="flex space-x-4 text-white">
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/livro">Livro</Link></li>
+        <li><Link href="/ideias">Ideia</Link></li>
+        <li><Link href="/teoria">Teoria</Link></li>
+      </ul>
+    </nav>
   );
 }
